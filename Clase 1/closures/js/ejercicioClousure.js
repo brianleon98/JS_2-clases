e82a2e1046af21f442c70bb67968436dc9100616
yonier999 <yonierjulian999@gmail.com>
2021-06-21 20:24:00 -0500

    ejercicios closures

--- Clase 1/closures/js/ejercicioClousure.js
+++ Clase 1/closures/js/ejercicioClousure.js
@@ -9,12 +9,83 @@ var suma10 = Sumador(10);
 console.log(suma5(2)); // sele da segunso valor a sumae
 console.log(suma10(2)); // muestra 12
 //==== 2
-function makeSizer(size) {
-    return function() {
-        document.body.style.fontSize = size + 'px';
-    };
+function mensaje(nombre) {
+    var msg = 'hola';
+    var respuesta = function() { alert(msg + ' ' + nombre); }
+    respuesta()
 }
 
-var size12 = makeSizer(12);
-var size14 = makeSizer(14);
-var size16 = makeSizer(16);
+function ejemplo() { mensaje('Juan'); }
+ejemplo();
+//==== 3
+function CrearContador() {
+    var contador = 0;
+    return function incrementar() {
+        contador = contador + 1;
+        return contador;
+    };
+}
+var contador = CrearContador();
+console.log(contador());
+
+//===== 4
+
+function operaciones(a, b) {
+    var suma = function() {
+        return a + b;
+    }
+    var resta = function() {
+        return a - b;
+    }
+    var multiplicacion = function() {
+        return a + b;
+    }
+    var division = function() {
+        return a + b;
+    }
+    return suma() + resta() + multiplicacion() + division(); // el resultado de las demas operaciones sera una suma de cada una
+}
+console.log("Resultado Operaciones suma", operaciones(2, 5));
+//====== 5
+var persona = {
+    nombre: "andres",
+    edad: 21,
+    pais: "mexico"
+}
+
+function mostrarTexto({ nombre, edad, pais }) { //parametros obtenidos por medio, destructuracion de objetos 
+    var texto = function() {
+        return `${nombre} tiene una edad de ${edad} años y es de ${pais}`;
+    }
+    return texto();
+}
+document.write(mostrarTexto(persona));
+// === 6
+
+function separarPalabrasLetras() {
+    var palabra = "BARRANQUILLA";
+    var letras = function() {
+
+        return palabra.split("");
+    }
+    return letras();
+}
+console.log("Separación", separarPalabrasLetras());
+//============= 7
+function PrimeraLetraNombre(name) {
+    var nombre = function() {
+        return name.charAt(); //parametro vacio toma por fecto la posicion 0
+    };
+    console.log("Primer letra de la palabra " + name + " es " + nombre());
+}
+PrimeraLetraNombre("Pablo");
+//============== 8
+function RemplazarT(nuevoT) { //palabra a cambiar por parametro
+    var contenido = "La vaca Lola"; //ctexto antiguo
+    var nuevoContenido = function() {
+        return contenido.replace("vaca", nuevoT); //recibe palabra a cambiar y nueva palabra
+    };
+    console.log(`Contenido antiguo: ${contenido}`, `Nuevo contenido: ${nuevoContenido()}`)
+}
+RemplazarT("Perro");
+//==================== 9
